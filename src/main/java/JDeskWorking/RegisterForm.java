0f837cbfcd82c0c1063e742c737e3bfb191b8a03
rishabhmaurya <rishabh_m26@outlook.com>
2023-10-01 12:13:41 +0530

    Testing to push pdf files inDB

--- src/main/java/JDeskWorking/RegisterForm.java
+++ src/main/java/JDeskWorking/RegisterForm.java
@@ -1,4 +1,12 @@
 package JDeskWorking;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class RegisterForm {
@@ -11,6 +19,25 @@ public class RegisterForm {
         userData.add(name);
         userData.add(mail);
         userData.add(password);
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/rishabhmaurya/Documents/SkyDesk/src/main/java/com/example/JDeskUI/userDetail.db");
+            Statement s = conn.createStatement();
+            s.execute("create table if not exists " +
+                    " userDetail(name text,email text,password text)");
+            s.execute("insert  into  userDetail(name,email,password)"+
+                    " values(,'hello@.com',2334)");
+            s.close();
+            conn.close();
+            System.out.println("success");
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+
+
 
     }
 
